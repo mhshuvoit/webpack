d290439d742a1a6ffb81e6414855810966078230
mhshuvoit <mhshuvoit@.com>
2020-07-18 18:54:34 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import "./styles.css";
+console.log("This is my first webpack run");
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  // mode: 'development',
+  // devServer: {
+  //   port: 3000,
+  //   open: true,
+  //   compress: true
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      filename: 'index.html'
+    }),
+    new LodashModuleReplacementPlugin,
+    new MiniCssExtractPlugin()
+  ]
+};
+
+module.exports = config;
